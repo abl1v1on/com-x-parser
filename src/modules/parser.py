c507749edf93691c2b848f31d438261c5dc0a37d
Maxim Danilov <danilovm16092005@gmail.com>
2026-01-22 08:56:47 +0300

    add collect metadata method

--- src/modules/parser.py
+++ src/modules/parser.py
@@ -1,10 +1,15 @@
+import json
+
+import httpx
 from selenium.webdriver import Chrome
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.remote.webelement import WebElement
 from selenium.common.exceptions import NoSuchElementException
+from webdriver_manager.chrome import ChromeDriverManager
+
+from utils import ask
+from config import settings
 
 
 class Parser:
@@ -16,6 +21,10 @@ class Parser:
 
     def collect_chapter_ids(self) -> list[str]:
         self.driver.get(self.url)
+
+        if ask("Collect metadata?"):
+            self.collect_metadata()
+
         self.to_chapters_btn.click()
         self.show_from_first_chapter_btn.click()
 
@@ -33,6 +42,49 @@ class Parser:
                 self.driver.quit()
                 break
         return chapter_ids
+
+    def collect_metadata(self) -> None:
+        settings.metadata_path.mkdir(exist_ok=True)
+        
+        poster_url = str(self.poster.get_attribute("src"))
+
+        # TODO: fix poster saving
+
+        with httpx.Client(
+            base_url=poster_url,
+            headers={
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/144.0.0.0 Safari/537.36",
+                "Host": "com-x.life",
+                "Cache-Control": "no-cache",
+            },
+        ) as client:
+            response = client.get("")
+
+            with open(
+                settings.metadata_path / "poster.jpg", 
+                mode="wb"
+            ) as file:
+                file.write(response.content)
+
+        with open(
+            settings.metadata_path / "data.json", 
+            mode="w",
+            encoding="utf-8",
+        ) as file:
+            clean_tags = [
+                tag.text.capitalize() 
+                for tag in self.tags
+            ]
+
+            content = json.dumps(
+                {
+                    "description": self.description.text,
+                    "tags": clean_tags,
+                },
+                indent=2,
+                ensure_ascii=False,
+            )
+            file.write(content)
 
     @property
     def to_chapters_btn(self) -> WebElement:
@@ -61,6 +113,28 @@ class Parser:
         return self._find(
             By.XPATH, 
             "//a[text()='Вперед']",
+        ) # type: ignore
+
+    @property
+    def poster(self) -> WebElement:
+        return self._find(
+            By.CSS_SELECTOR, 
+            ".page__poster img",
+        ) # type: ignore
+
+    @property
+    def description(self) -> WebElement:
+        return self._find(
+            By.CSS_SELECTOR,
+            ".page__text",
+        ) # type: ignore
+
+    @property
+    def tags(self) -> list[WebElement]:
+        return self._find(
+            By.CSS_SELECTOR,
+            ".page__tags a",
+            many=True
         ) # type: ignore
 
     def _find(

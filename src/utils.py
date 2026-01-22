3d42a4b795b9073e7b23344d560f5686bdd76739
Maxim Danilov <danilovm16092005@gmail.com>
2026-01-22 09:10:36 +0300

    add get_user_agent method

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,13 @@
+from fake_useragent import UserAgent
+
+
 def ask(question: str) -> bool:
     answer = input(f"{question} [Y/n]: ").lower().strip()
 
     if answer in {"y", "yes", ""}:
         return True
     return False
+
+
+def get_user_agent() -> str:
+    return UserAgent().random

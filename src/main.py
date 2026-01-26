e47b365b081548a310c779dd7c204a028b5656b1
Maxim Danilov <danilovm16092005@gmail.com>
2026-01-26 20:20:38 +0300

    use modules

---
+++ src/main.py
@@ -0,0 +1,31 @@
+import asyncio
+
+from utils import ask
+from config import settings
+from modules.parser import Parser
+from modules.auth import AuthModule
+from modules.downloader import Downloader
+
+
+async def main() -> None:
+    auth_module = AuthModule()
+
+    if (
+        not settings.login.cookies_path.exists() 
+        or ask("Save cookies again?")
+    ):
+        await auth_module.save_auth_cookies()
+
+    manga_url = str(input("Manga URL: "))
+    parser = Parser(manga_url)
+    chapter_ids = parser.collect_chapter_ids()
+
+    downloader = Downloader()
+    full_chapter_ids = await downloader.get_full_chapter_ids(
+        parser.news_id, chapter_ids
+    )
+    await downloader.download_chapters(full_chapter_ids)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
